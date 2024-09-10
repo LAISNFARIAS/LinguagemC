9028a7787d337ec1f32caaeb87999f4707d91946
Laís <laisnf001@gmail.com>
2024-09-10 14:41:58 +0000

    Primeiro

---
+++ Primeiro.c
@@ -0,0 +1,11 @@
+ #include <stdio.h> // biblioteca standard de Input/Output
+ int main()
+ {
+ int j;  //declarar var, j inteira
+ printf("Digite um valor inteiro e Enter\n");
+ scanf("%d", &j);
+ printf("Valor de j é %d\n",j);
+ return 0; 
+
+
+ }
