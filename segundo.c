644b854e95941848b67301901fee0f693ba6d9a3
Laís <laisnf001@gmail.com>
2024-09-11 11:40:36 +0000

    Início do estudo em C

--- segundo.c
+++ segundo.c
@@ -2,7 +2,7 @@
 int main(){
 
 char car;
-printf("Digite um caractere e Enter\n");
+printf("Digite um caractere e pressione Enter\n");
 scanf("%c", &car);
 printf("Caractere digitado é %c\n", car);
 return 0;
