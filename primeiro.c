77846fb411a16f425efdbe71c2b67e8499455751
Laís <laisnf001@gmail.com>
2024-09-11 11:39:42 +0000

    Início do estudo em C

--- primeiro.c
+++ primeiro.c
@@ -2,7 +2,7 @@
  int main()
  {
  int j;  //declarar var, j inteira
- printf("Digite um valor inteiro para iniciar e Enter\n");
+ printf("Digite um valor inteiro para iniciar e digite Enter\n");
  scanf("%d", &j);
  printf("Valor de j é %d\n",j);
  return 0;
