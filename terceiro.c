a55c190575e25dba0ce3d8e18adb9d84e07a42b6
Laís <laisnf001@gmail.com>
2024-09-11 11:50:09 +0000

    Início do estudo em C

--- terceiro.c
+++ terceiro.c
@@ -7,7 +7,7 @@ int main(){
 float raio, area;
 printf("Digite o valor do raio do círculo\n");
 scanf("%f", &raio);
-printf("O raio digitado é %12.2f\n",raio);
+printf("Raio digitado é %12.2f\n",raio);
 area=PI*raio*raio;
 printf("Área do círculo com raio %f é %f\n", raio, area);
 return 0;
